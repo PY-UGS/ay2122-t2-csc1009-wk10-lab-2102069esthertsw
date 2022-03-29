4434aadc6a3bc45ab9c30614be80c1d9f31ba5c8
Esther <98366701+2102069esthertsw@users.noreply.github.com>
2022-03-29 11:50:05 +0800

    Update Q2Average.py

--- Q2Average.py
+++ Q2Average.py
@@ -1,6 +1,11 @@
 print("Function to find the average of two numbers\n")
 
+//prompt user for input
 x = int(input("\nEnter the first number: "))
 y = int(input("\nEnter the second number: "))
+
+//compute average
 ans = float((x+y) /2)
+
+//display answer
 print("\nThe average is ", ans)

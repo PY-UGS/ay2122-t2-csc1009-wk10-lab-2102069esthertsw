b7fb49804f728034d146b4e5c794cb1990ac444f
Esther <98366701+2102069esthertsw@users.noreply.github.com>
2022-03-29 11:52:14 +0800

    Update Q3RepeatLab1Q2c.py

--- Q3RepeatLab1Q2c.py
+++ Q3RepeatLab1Q2c.py
@@ -1,6 +1,9 @@
+# prompt user for max and min integers
 max = int(input("Enter your maximum integer: "))
 min = int(input("Enter your minimum integer: "))
+
 x = max
 while(x>=min):
+#     print values from max to min, decrement by 2
     print("value of x: " , x )
     x-=2
